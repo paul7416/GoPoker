751821651ad7455e4a099ea1870385b58c2920ba
Paul Brown <paulmbrown200@gmail.com>
2026-01-30 16:27:28 +1300

    implemented fisher-yates preshuffle (properly this time)

--- lib/ev_calculator.h
+++ lib/ev_calculator.h
@@ -2,6 +2,14 @@
 #include "evaluator.h"
 #include "histogram.h"
 
+#define DECK_SIZE 52
+
+typedef struct{
+    uint8_t card_array[DECK_SIZE];
+    uint8_t current_index;
+    uint8_t number_of_shuffled_cards;
+    uint8_t padding[2];
+}cardDeck;
 
 // Xorshift128+ state (must be seeded with non-zero values)
 void iterator(const int iterations, GameState *G, HistogramTable *H, const evaluatorTables *T);
--- src/ev_calculator.c
+++ src/ev_calculator.c
@@ -5,8 +5,19 @@
 #include <assert.h>
 //#include "debug.h"
 
-// Xorshift128+ generator
+cardDeck create_card_deck(int no_players)
+{
+    cardDeck d;
+    d.current_index = 0;
+    for(int i = 0; i < DECK_SIZE; i++)
+    {
+        d.card_array[i] = i;
+    }
+    d.number_of_shuffled_cards = 2 * no_players + 5;
+    return d;
+}
 
+// Xorshift128+ generator
 static inline uint64_t xorshift128plus(uint64_t s[2]) {
     uint64_t x = s[0];
     uint64_t y = s[1];
@@ -21,75 +32,73 @@ void seed(uint64_t s[2])
     for( int i = 0; i < 20; i++)xorshift128plus(s);
 }
 
-static inline uint64_t generate_community_cards(uint64_t *s)
+static inline void shuffle_deck(cardDeck *d, uint64_t s[2])
 {
-    uint64_t cards = 0;
-    int count = 0;
-    while (count < 5)
+    uint32_t tmp;
+    for(int i = 0; i < d->number_of_shuffled_cards; i++)
     {
-        //int card = xorshift128plus(s) % 52;
-        int card = ((uint64_t)((uint32_t)xorshift128plus(s)) * 52) >> 32;
-        uint64_t bit = 1ULL << card;
-        if (!(bit & cards))
-        {
-            cards |= bit;
-            count++;
-        }
+        uint32_t rnd_index = (((uint64_t)((uint32_t)xorshift128plus(s)) * (DECK_SIZE - i)) >> 32) + i;
+        tmp = d->card_array[i];
+        d->card_array[i] = d->card_array[rnd_index];
+        d->card_array[rnd_index] = tmp;
     }
-    return cards;
+
+}
+
+
+
+static inline uint64_t generate_community_cards(cardDeck *d)
+{
+    d->current_index = 5;
+    return (1ull << d->card_array[0])|
+           (1ull << d->card_array[1])|
+           (1ull << d->card_array[2])|
+           (1ull << d->card_array[3])|
+           (1ull << d->card_array[4]);
 }
 
 void iterator(const int iterations, GameState *G, HistogramTable *H, const evaluatorTables *T)
 {
     // Build lightweight copy
-    uint16_t local_playable_hands[0x1000] = {0};
+    uint16_t local_playable_hands[0x3400] = {0};
     GameStateSim sim;
     sim.no_players = G->no_players;
     for (int i = 0; i < sim.no_players; i++)
     {
-        for(int j = 0; j < 0x1000; j++)
+        for(int j = 0; j < 0x3400; j++)
         {
             local_playable_hands[j] |= (G->players[i].range.playableHands[j] << i);
         }
         sim.players[i].index = G->players[i].index;
     }
+    cardDeck d = create_card_deck(G->no_players);
     
+    // create and seed rng
     uint64_t s[2];
     seed(s);
-    uint64_t card_1_mask, card_2_mask, used;
+
+    // loop variables
     uint8_t active_count, last_active;
-    uint16_t card_1_index, card_2_index;
+
 
     for(int iteration = 0; iteration < iterations; iteration++)
     {
+        shuffle_deck(&d, s);
         active_count = 0;
         last_active = 0;
-        sim.community_cards = generate_community_cards(s);
-        used = sim.community_cards;
+        sim.community_cards = generate_community_cards(&d);
+        uint16_t *player_draw_cards = (uint16_t*)(d.card_array + 5);
         for(int i = 0; i < sim.no_players; i++)
         {
             PlayerSim *p = &sim.players[i];
-
-            while(1)
+            assert(player_draw_cards[i] < 0x3400);
+            p->folded = !(local_playable_hands[player_draw_cards[i]] & (1 << i));
+            if (!p->folded)
             {
-                // Lemire's fast range mapping (biased, but negligible for Monte Carlo)
-                card_1_index = ((uint64_t)((uint32_t)xorshift128plus(s)) * 52) >> 32;
-                card_1_mask = 1ull << card_1_index;
-                if(!(card_1_mask&used))break;
+                p->hole_cards = (1ull << d.card_array[5 + 2 * i])|(1ull << d.card_array[6 + 2 * i]);
+                active_count++;
+                last_active = i;
             }
-            used |= card_1_mask;
-            // After getting card_1_index, before second card loop:
-            while(1)
-            {
-                card_2_index = ((uint64_t)((uint32_t)xorshift128plus(s)) * 52) >> 32;
-                card_2_mask = 1ull << card_2_index;
-                if(!(card_2_mask & used))break;
-            }
-            used |= card_2_mask;
-            p->hole_cards = card_1_mask|card_2_mask;
-            p->folded = !(local_playable_hands[card_1_index << 8 | card_2_index] & (1 << i));
-            active_count += !p->folded;
-            last_active = i * (!p->folded) +(last_active * p->folded);
         }
         sim.last_active = last_active;
         sim.active_count = active_count;
